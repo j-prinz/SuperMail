a435204d4bb5c2cb320778f0e0d8ac81867d7b43
j-prinz <jprinzie@gmail.com>
2020-04-01 22:59:55 -0700

    fixed create ddl

--- SuperMail/src/db/create.sql
+++ SuperMail/src/db/create.sql
@@ -20,21 +20,21 @@ CREATE TABLE Items (
   Fragility CHAR(6),
   FOREIGN KEY (Order_ID) REFERENCES DeliveryOrder(Order_ID) ON DELETE
   CASCADE
-)
+);
 
 CREATE TABLE Customer (
   Customer_ID INT PRIMARY KEY,
   Customer_Name CHAR(20),
   Customer_Phone INT,
-  Customer_Address CHAR(100),
-)
+  Customer_Address CHAR(100)
+);
 
 CREATE TABLE Receiver (
   Receiver_ID INT PRIMARY KEY,
   Receiver_Name CHAR(20),
   Receiver_Address CHAR(100),
-  Receiver_Postal_Code CHAR(6),
-)
+  Receiver_Postal_Code CHAR(6)
+);
 
 CREATE TABLE DeliveryOrder (
   Order_ID INT PRIMARY KEY,
@@ -45,44 +45,43 @@ CREATE TABLE DeliveryOrder (
   Delivery_Status CHAR(10),
   Pricing REAL,
   Receiver_ID INT,
-  FOREIGN KEY Customer_ID REFERENCES Customer(CUSTOMER_ID) ON DELETE
-   NO ACTION,
-  FOREIGN KEY EID REFERENCES Employee ON DELETE NO ACTION,
-  FOREIGN KEY Reciever_ID REFERENCES Receiver ON DELETE NO ACTION,
-)
+  FOREIGN KEY (Customer_ID) REFERENCES Customer(CUSTOMER_ID),
+  FOREIGN KEY (EID) REFERENCES Employee(EID),
+  FOREIGN KEY (Receiver_ID) REFERENCES Receiver(Receiver_ID)
+);
 
 CREATE TABLE Employee (
   EID INT PRIMARY KEY,
   EName CHAR(20),
-  EPhone INT,
-)
+  EPhone INT
+);
 
 CREATE TABLE EmployeeWorkArea (
   EID INT PRIMARY KEY,
   Work_Area CHAR(20),
-  FOREIGN KEY EID REFERENCES Employee(EID) ON DELETE CASCADE
-)
+  FOREIGN KEY (EID) REFERENCES Employee(EID) ON DELETE CASCADE
+);
 
 CREATE TABLE MailboxServicedBy(
   MBID INT PRIMARY KEY,
   POID INT,
-  FOREIGN KEY POID REFERENCES PostOfficeDetails ON DELETE SET NULL,
-)
+  FOREIGN KEY (POID) REFERENCES PostOfficeDetails(POID) ON DELETE SET NULL
+);
 
 CREATE TABLE PostOffice (
   Receiver_Postal_Code CHAR(6) PRIMARY KEY,
   POID INT,
-  FOREIGN KEY POID REFERENCES PostOfficeDetails ON DELETE SET NULL,
-)
+  FOREIGN KEY (POID) REFERENCES PostOfficeDetails(POID) ON DELETE SET NULL
+);
 
 CREATE TABLE AreaServicedByPO (
   Work_Area CHAR(20) PRIMARY KEY,
   POID INT,
-  FOREIGN KEY Work_Area REFERENCES EmployeeWorkArea ON DELETE SET NULL,
-)
+  FOREIGN KEY (Work_Area) REFERENCES EmployeeWorkArea(Work_Area) ON DELETE SET NULL
+);
 
 CREATE TABLE PostOfficeDetails (
   POID INT PRIMARY KEY,
-  Locaon CHAR(20),
-  City CHAR(20),
+  Location CHAR(20),
+  City CHAR(20)
 )
